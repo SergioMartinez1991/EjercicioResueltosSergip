4dfd4e70b96dd212dc4f0567882d3404152be7a1
SergioMartinez1991 <sergiomartinezcolumna@gmail.com>
2024-07-17 10:11:27 -0600

    Ejercicio01

---
+++ app/src/main/java/com/example/ejerciciosresueltossergio/PostWord.kt
@@ -0,0 +1,11 @@
+package com.example.ejerciciosresueltossergio
+
+import kotlin.math.PI
+
+fun calcularVolumenCilindro(radio: Double, altura: Double): Double {
+    return PI * radio * radio * altura
+}
+
+fun main() {
+    println("Volumen del cilindro: ${calcularVolumenCilindro(5.0, 10.0)}")
+}
